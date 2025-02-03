5cf316acd85e0f6037f5ad2d367fd5d1e062ca6a
Pragati <pragati.dhasmana99@gmail.com>
2025-02-03 18:50:17 +0530

    added cartitem

--- E-Commerance-Website/Models/CartItem.cs
+++ E-Commerance-Website/Models/CartItem.cs
@@ -1,8 +1,18 @@
-﻿namespace E_Commerance_Website.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_Commerance_Website.Models
 {
     public class CartItem
     {
-        public string Id { get; set; }  
+        public int Id { get; set; }
+        [ForeignKey("Products")]
+        [Required]
+        public int ProductId { get; set; }
+        public string Name { get; set; }    
+        public decimal Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
 
     }
 }
